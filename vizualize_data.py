ec77e615dcb6f3b0b25220ec2fd9ead0b75584e7
[Timothy Reitz] <[reitz.timothy.w@gmail.com]>
2021-03-20 08:49:24 -0400

    Fixed joshua issues

--- vizualize_data.py
+++ vizualize_data.py
@@ -11,10 +11,11 @@ def read_data(filename):
     return year_data
 
 def graphyears(number_of_years,start_year,dates):
+
     Months = ['January','March','May','July','September','November','January']
     labels=[]
-    fig , ax = plt.subplots(figsize=(40,4))
-    for i in range(0,number_of_years):
+    ax = plt.subplots(figsize=(40,4))
+    for counter in range(0,number_of_years):
         data=read_data(str(start_year)+'year.npy')
         labels.append(str(start_year))
         ax.plot(dates[0],data[0],linewidth=2.5)
@@ -29,6 +30,7 @@ def graphyears(number_of_years,start_year,dates):
     plt.show()
 
 def get_dates_for_graphing(startYear=1111):
+
     startDay = '01'
     endDay = '01'
     dateslist = []
@@ -39,7 +41,9 @@ def get_dates_for_graphing(startYear=1111):
     dateslist.append(np.arange(startDate , endDate , dtype='datetime64[D]'))
     dateslist = np.array(dateslist)
     return dateslist
+
 def make_dates():
+
     dates=[]
     Months=['01','02','03','04','05','06','07','08','09','10','11','12']
     thirtydaymonths=['09','04','06','11']
@@ -57,11 +61,13 @@ def make_dates():
     return dates
 
 def main():
+
     number_of_years=input("Enter how many Years you want to graph: ")
     start_year=input('Please enter a starting year: ')
     #dates=make_dates()
     dates = get_dates_for_graphing()
     print(dates.shape)
     graphyears(int(number_of_years),int(start_year),dates)
+
 if __name__ == '__main__':
     main()
