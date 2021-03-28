4a3b9b5a5ddf24aca835306a3f1bedd23ec5a8bc
Eron <eyneill777@gmail.com>
2021-03-28 19:38:50 -0400

    Tried out a bunch of things with the model.  Got validation set working, implemented skip layers and sequential models.

--- Model.py
+++ Model.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten, concatenate, Reshape, Dropout
 from tensorflow.keras.models import Model
 
 def getAutoEncoder(input_shape):
@@ -27,7 +27,6 @@ def getAutoEncoder(input_shape):
     ###############
     # Forecasting #
     ###############
-    
     x=Dense(units = 45, activation = 'relu')(encoded)
     forecast = Dense(units = 45, activation = 'relu')(x)
     
@@ -51,3 +50,65 @@ def getAutoEncoder(input_shape):
     decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
     
     return Model(input_img, decoded)
+    
+    
+def getSequentialAutoEncoder(input_shape):
+
+    inputs = []
+    encoded = []
+    for i in range(input_shape[2]):
+        # The encoding process
+        inputs.append(Input(shape=(input_shape[0], input_shape[1], 1)))
+
+        ############
+        # Encoding #
+        ############
+
+        # Conv1 #
+        x = Conv2D(filters = 16, kernel_size = (3, 3), activation='relu', padding='same')(inputs[i])
+        x = MaxPooling2D(pool_size = (2, 2), padding='same')(x)
+
+        # Conv2 #
+        x = Conv2D(filters = 8, kernel_size = (3, 3), activation='relu', padding='same')(x)
+        x = MaxPooling2D(pool_size = (2, 2), padding='same')(x) 
+
+        # Conv 3 #
+        x = Conv2D(filters = 8, kernel_size = (3, 3), activation='relu', padding='same')(x)
+        encoded.append(MaxPooling2D(pool_size = (2, 2), padding='same')(x))
+
+        
+    
+    ###############
+    # Forecasting #
+    ###############
+    x = Flatten()(encoded[0])
+    x=Dense(units = 500, activation = 'relu')(x)
+    x=Dropout(rate = .5)(x)
+    for i in range(input_shape[2]-1):
+        flt = Flatten()(encoded[i+1])
+        combined = concatenate([x, flt])
+        x=Dense(units = 500+400*(i+1), activation = 'relu')(combined)
+        x=Dropout(rate = .5)(x)
+    forecast = Dense(units = 2025, activation = 'relu')(x)
+    forecast = Reshape((45,45, 1))(forecast)
+    
+    
+
+    ############
+    # Decoding #
+    ############
+
+    # DeConv1
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(forecast)
+    x = UpSampling2D((2, 2))(x)
+
+    # DeConv2
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+
+    # Deconv3
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    decoded = Conv2D(1, (3, 3), activation='relu', padding='same')(x)
+    
+    return Model(inputs, decoded)
