b3a92cde7ddee38631fae970dab5255004548bae
[Timothy Reitz] <[reitz.timothy.w@gmail.com]>
2021-04-06 12:37:13 -0400

    Added average over year graph.

--- vizualize_data.py
+++ vizualize_data.py
@@ -16,8 +16,7 @@ def graphyears(number_of_years,start_year):
     labels=[]
 
     dates = get_dates_for_graphing()
-    print(dates)
-    fig, ax = plt.subplots(figsize=(40,4))
+    fig, ax = plt.subplots(figsize=(17,4))
 
     for counter in range(0,number_of_years):
         data=read_data(str(start_year)+'year.npy')
@@ -30,6 +29,8 @@ def graphyears(number_of_years,start_year):
     ax.legend(labels,fancybox=True, shadow=True)
     ax.grid()
     plt.title('Precipitation over a year')
+    plt.xlabel('Months')
+    plt.ylabel('Total precipitation')
     plt.savefig('Year.png')
     plt.show()
 
@@ -79,7 +80,7 @@ def average_month(year_Data):
             average= total / 30
             averages.append(average)
         elif Month == '02':
-            for day in range(0 , 2):
+            for day in range(0 , 28):
                 total += year_Data[0][count]
                 count += 1
             average = total / 29
@@ -98,7 +99,7 @@ def graph_month_average(number_of_years,start_year):
     Months = ['January' ,'Febuary', 'March' ,'April', 'May' ,'June', 'July' ,'August', 'September' ,'October', 'November' ,'December']
     labels = []
     dates = make_months_for_graphing()
-    fig, ax = plt.subplots(figsize=(40 , 4))
+    fig, ax = plt.subplots(figsize=(12.5 , 4))
     for counter in range(0 , number_of_years):
         data = read_data(str(start_year) + 'year.npy')
         data=average_month(data)
@@ -108,6 +109,8 @@ def graph_month_average(number_of_years,start_year):
     ax.set_xticklabels(Months)
     ax.legend(labels , fancybox=True , shadow=True)
     ax.grid()
+    plt.xlabel('Months')
+    plt.ylabel('Total precipitation')
     plt.title('Average precipitation by month over a year')
     plt.savefig('Month_Average.png')
     plt.show()
@@ -116,12 +119,57 @@ def make_months_for_graphing():
     Months = ['01' , '02' , '03' , '04' , '05' , '06' , '07' , '08' , '09' , '10' , '11' , '12']
     Months = np.array([Months])
     return Months
+
+def average_year(year_Data):
+    total = 0
+    for index in range(0,len(year_Data[0])):
+        total+=year_Data[0][index]
+    return (total/len(year_Data[0]))
+
+
+
+def make_years_for_graphing(number_of_years,start_year):
+    years=[]
+    for i in range (number_of_years):
+        years.append(start_year)
+        start_year+=1
+    years = np.array(years)
+    return years
+
+def graph_average_of_years(number_of_years,start_year):
+    Months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+              'November', 'December']
+    labels = []
+    dates = make_years_for_graphing(number_of_years,start_year)
+    year_data=[]
+    fig, ax = plt.subplots(figsize=(12.5, 4))
+    for counter in range(0, number_of_years):
+        data = read_data(str(start_year) + 'year.npy')
+        year_data.append(average_year(data))
+        labels.append(str(start_year))
+        start_year += 1
+    year_data=np.array(year_data)
+    print(dates,year_data)
+    ax.plot(dates, year_data, linewidth=2.5)
+    ax.grid()
+    plt.locator_params(axis='x', nbins=number_of_years)
+    plt.xlabel('Years')
+    plt.ylabel('Total precipitation')
+    ax.legend(['Average'], fancybox=True, shadow=True)
+    plt.title('Average precipitation by year')
+    plt.savefig('Year_average.png')
+    plt.show()
+
 def main():
+
 
     number_of_years=int(input("Enter how many Years you want to graph: "))
     start_year=int(input('Please enter a starting year: '))
+    graph_average_of_years(number_of_years,start_year)
+'''
     graphyears(number_of_years,start_year)
     graph_month_average(number_of_years,start_year)
+'''
 
 if __name__ == '__main__':
     main()
