f6ce83a4ca0bc4e69afa10730a90d28ddaff85ed
Eron <eyneill777@gmail.com>
2021-04-14 15:21:28 -0400

    Major changes to models and training as well as new visualization functions

--- FoliumPrecipitation.py
+++ FoliumPrecipitation.py
@@ -4,11 +4,17 @@ import folium
 import numpy as np
 import geemap
 import pandas as pd
-import DataProcessing
 import sklearn
 from sklearn.preprocessing import MinMaxScaler
 import os
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import tensorflow.keras as keras
+import tensorflow as tf
+import Model
+from tensorflow.keras.losses import Loss as Loss
+from datetime import date, timedelta
+
 
 #Get an authentication token from google, do every time if running on the cloud, do first time only if running local
 #ee.Authenticate()
@@ -146,16 +152,17 @@ def batch_numpy_file_to_folder(input_filename, save_folder, batch_size, sequence
     print(maps[:,:,0])
     
     
-    num_batches = int((maps.shape[2]-sequence_length)/batch_size)
+    num_batches = int((maps.shape[2]-sequence_length*2)/batch_size)
     print(num_batches)
     for b in range(num_batches):
         print(b)
-        inp = np.empty(shape = (batch_size, maps.shape[0], maps.shape[1], sequence_length))
-        out = np.empty(shape = (batch_size, maps.shape[0], maps.shape[1],  1))
+        inp = np.empty(shape = (batch_size, sequence_length, maps.shape[0], maps.shape[1]))
+        out = np.empty(shape = (batch_size, sequence_length, maps.shape[0], maps.shape[1]))
         for i in range(batch_size):
             for s in range(sequence_length):
-                inp[i,:,:,s] = maps[:,:,b*batch_size+i+s]
-            out[i,:,:,0] = maps[:,:,b*batch_size+i+sequence_length]
+                inp[i,s,:,:] = maps[:,:,b*batch_size+i+s]
+            for s in range(sequence_length):
+                out[i,s,:,:] = maps[:,:,b*batch_size+i+sequence_length+s]
         
         np.save(fp+save_folder+"\\input"+str(b), inp)
         np.save(fp+save_folder+"\\output"+str(b), out)
@@ -174,16 +181,17 @@ def batch_numpy_file_to_folder_with_normalizer(input_filename, save_folder, batc
     print(maps[:,:,0])
     
     
-    num_batches = int((maps.shape[2]-sequence_length)/batch_size)
+    num_batches = int((maps.shape[2]-sequence_length*2)/batch_size)
     print(num_batches)
-    for b in range(num_batches):
+    for b in range(num_batches-1):
         print(b)
-        inp = np.empty(shape = (batch_size, maps.shape[0], maps.shape[1], sequence_length))
-        out = np.empty(shape = (batch_size, maps.shape[0], maps.shape[1],  1))
+        inp = np.empty(shape = (batch_size, sequence_length, maps.shape[0], maps.shape[1]))
+        out = np.empty(shape = (batch_size, sequence_length, maps.shape[0], maps.shape[1]))
         for i in range(batch_size):
             for s in range(sequence_length):
-                inp[i,:,:,s] = maps[:,:,b*batch_size+i+s]
-            out[i,:,:,0] = maps[:,:,b*batch_size+i+sequence_length]
+                inp[i,s,:,:] = maps[:,:,b*batch_size+i+s]
+            for s in range(sequence_length):
+                out[i,s,:,:] = maps[:,:,b*batch_size+i+sequence_length+s]
         
         np.save(fp+save_folder+"\\input"+str(b), inp)
         np.save(fp+save_folder+"\\output"+str(b), out)
@@ -203,10 +211,106 @@ def viualize_data(dataset):
     print("this is your code: ")
     print(dataset)
 
+def get_data_stats():
+    data = np.load("H:\\Datasets\\ChirpsDaily\\ChirpsDaily.npy")
+    avg = np.average(data, 2)
+    std = np.std(data, axis = 2)
+    #display_single_image_plot(avg)
+    #display_single_image_plot(std)
+    return (avg, std)
 
+def on_click(event):
+    print(str([event.xdata,event.ydata]))
 
+def display_single_image_plot(im):
+    plt.imshow(im)
+    plt.canvas.mpl_connect('button_press_event', on_click)
+    plt.show()
 
+def get_split_averages_on_data(num_splits):
+    data = np.load("H:\\Datasets\\ChirpsDaily\\ChirpsDaily.npy")
+    splits = []
+    split_len = (int)(data.shape[2]/num_splits)
+
+    for s in range(num_splits):
+        splits.append(np.average(data[:,:,s*split_len:(s+1)*split_len], axis = 2))
+    return splits
     
+def get_split_stdevs_on_data(num_splits):
+    data = np.load("H:\\Datasets\\ChirpsDaily\\ChirpsDaily.npy")
+    splits = []
+    split_len = (int)(data.shape[2]/num_splits)
+
+    for s in range(num_splits):
+        splits.append(np.std(data[:,:,s*split_len:(s+1)*split_len], axis = 2))
+    return splits
+
+def visualize_splits_vs_avg(splits, avg, width, height):
+    f, im = plt.subplots(width,height)
+
+    for i in range(len(splits)):
+        dat = np.subtract(splits[i], avg)
+        n = colors.TwoSlopeNorm(vcenter = 0)
+        if height > 1:
+            im[i%width, int(i/width)].imshow(dat, cmap = 'seismic', norm = n)
+        else:
+            im[i].imshow(dat, cmap = 'seismic', norm = n)
+    
+    plt.show()
+
+def visualize_splits_vs_prev(splits, width, height):
+    f, im = plt.subplots(width,height)
+
+    for i in range(len(splits)):
+        n = colors.TwoSlopeNorm(vcenter = 0)
+        if(i>0):
+            dat = np.subtract(splits[i], splits[i-1])
+        else:
+            dat = splits[i]
+        if height > 1:
+            im[i%width, int(i/width)].imshow(dat, cmap = 'seismic', norm = n)
+        else:
+            im[i].imshow(dat, cmap = 'seismic', norm = n)
+    
+    plt.show()   
+
+def visualize_splits_vs_first(splits, width, height):
+    f, im = plt.subplots(width,height)
+
+    for i in range(len(splits)):
+        n = colors.TwoSlopeNorm(vcenter = 0)
+        dat = np.subtract(splits[i], splits[0])
+        if height > 1:
+            im[i%width, int(i/width)].imshow(dat, cmap = 'seismic', norm = n)
+        else:
+            im[i].imshow(dat, cmap = 'seismic', norm = n)
+    
+    plt.show()    
+    
+def generate_empty_data(batch_size):
+    dat = np.zeros(shape = (batch_size,1,360,360))
+    #dat[int(batch_size/2):batch_size,:,:,:] = 1
+    np.save("H:\\Datasets\\ChirpsDaily\\PreBatched\\EmptyData\\empty", dat)
+
+def getDateVector(start_date, days, sequence_len, ):
+    data = np.zeros((days, sequence_len, 13))
+    for i in range(days):
+        for s in range(sequence_len):
+            date = start_date+timedelta(days = i+s)
+            data[i,s,(date.month-1)] = 1
+            data[i,s,12] = date.year-start_date.year/40
+    date = start_date+timedelta(days = days)
+    return (data, date)
+
+
+
+
+
+
+
+
+
+
 
 #Examples   
  
@@ -328,12 +432,12 @@ def ex5(test_file_number):
     
     plt.show()
     
-#Ex 6) Train sequential autoencoder model on pre batched files
+#Ex 6) Train sequential autoencoder model on pre batched files - Deprecated, data format changed
 def ex6(sequence_length):
     import tensorflow as tf
     import Model
     
-    epochs = 10
+    epochs = 3
     sequence_length = 5
     
     
@@ -342,8 +446,8 @@ def ex6(sequence_length):
     validation_batch_count = int(len(os.listdir(data_path+"Validation\\"))/2)
     print(batch_count)
     
-    model = tf.keras.models.load_model('SequentialAutoEncoder.model') #Model.getSequentialAutoEncoder((360, 360, sequence_length))
-    model.compile(optimizer='adam', loss='mse')
+    model = Model.getSequentialAutoEncoder((360, 360, sequence_length))#tf.keras.models.load_model('SequentialAutoEncoder.model') 
+    model.compile(optimizer='adam', loss='binary_crossentropy')
     print(model.summary())
     
     for e in range(epochs):
@@ -376,7 +480,7 @@ def ex6(sequence_length):
         
     model.save('SequentialAutoEncoder.model')
 
-#Ex 7) Load sequentialautoencoder model and make a prediction from the test set
+#Ex 7) Load sequentialautoencoder model and make a prediction from the test set - Deprecated, data format changed
 def ex7(test_file_number):
     import tensorflow.keras as keras
 
@@ -400,14 +504,264 @@ def ex7(test_file_number):
     im[6].imshow(out[0,:,:,0])
     
     plt.show()
+    
+#Ex 8) Train single day 1-1 autoencoder model on pre batched numpy files
+def ex8():
+    import tensorflow as tf
+    import Model
+    import Losses
+    
+    epochs = 15
+    zeros_frequency = 100
+
+    data_path = "H:\\Datasets\\ChirpsDaily\\PreBatched\\"
+    batch_count = int(len(os.listdir(data_path+"Train\\"))/2)
+    print(batch_count)
+    
+    ensemble = Model.getSameDayAutoEncoder((360,360,1))
+    
+    
+    model = ensemble[0] 
+    model.compile(optimizer='adam', loss='mae')
+    print(model.summary())
+    
+    zeros = np.reshape(np.load(data_path+"EmptyData\\"+"empty.npy"), (-1,360,360,1))
+    zerout = np.empty(zeros.shape)
+    
+    for e in range(epochs):
+        print("Epoch "+str(e))
+        for b in range(batch_count):
+            inp = np.reshape(np.load(data_path+"Train\\"+"input"+str(b)+".npy"), (-1,360,360,1))
+            out = np.reshape(np.load(data_path+"Train\\"+"input"+str(b)+".npy"), (-1,360,360,1))
+            
+            if(b%zeros_frequency == 0):
+                print(str(model.train_on_batch(zeros, zeros, return_dict = True)) +" on noise")
+            if b %(batch_count/8) == 0:
+                print(model.train_on_batch(inp, out, return_dict = True))
+            else:
+                model.train_on_batch(inp, out, return_dict = True)
+    #Note that the values that will be printed out are not the overall loss for the epoch, rather they are the loss for that specific batch.  So it will fluctuate a bit as it finds batches that it performs better or worse on    
+        
+    model.save('SameDayAutoEncoder.model')
+    ensemble[1].save('encoder.model')
+    ensemble[2].save('decoder.model')
+    
+#Ex 9) Load single day 1-1 autoencoder model and make a prediction from the test set
+def ex9(test_file_number):
+
+    model = keras.models.load_model('AutoEncoder.model')
+    
+    data_path = "H:\\Datasets\\ChirpsDaily\\PreBatched\\"
+    inp = np.reshape(np.load(data_path+"Test\\"+"input"+str(test_file_number)+".npy"), (-1,360,360,1))
+    out = np.reshape(np.load(data_path+"Test\\"+"input"+str(test_file_number)+".npy"), (-1,360,360,1))
+    
+    pred = model.predict_on_batch(inp)
+    f, im = plt.subplots(2,2)
+    im[0,0].set_title("Input Data")
+    im[0,0].imshow(inp[0,:,:,0])
+    im[1,1].set_title("Ground Truth")
+    im[1,1].imshow(out[0,:,:,0])
+    im[1,0].set_title("Next Day Prediction")
+    im[1,0].imshow(pred[0,:,:,0])
+    
+    plt.show()
+
+#Ex 10) Load pre-encoded model and train the forecasting bottleneck - Deprecated, data format changed
+def ex10(sequence_length):
+    import tensorflow as tf
+    import Model
+    
+    epochs = 3
+    sequence_length = 5
+    
+    
+    data_path = "H:\\Datasets\\ChirpsDaily\\Sequential\\"
+    batch_count = int(len(os.listdir(data_path+"Train\\"))/2)
+    validation_batch_count = int(len(os.listdir(data_path+"Validation\\"))/2)
+    print(batch_count)
+    
+    model = Model.getSequentialPreEncoded((360, 360, sequence_length))#tf.keras.models.load_model('SequentialPreEncoded.model') 
+    model.compile(optimizer='adam', loss='binary_crossentropy')
+    print(model.summary())
+    
+    for e in range(epochs):
+        print("Epoch "+str(e))
+        epoch_loss = 0;
+        validation_loss = 0;
+        for b in range(batch_count):
+            inputs = []
+            inp = np.load(data_path+"Train\\"+"input"+str(b)+".npy")
+            for i in range(inp.shape[3]):
+                inputs.append(inp[:,:,:,i])
+            out = np.load(data_path+"Train\\"+"output"+str(b)+".npy")
+            batch_loss = model.train_on_batch(inputs, out, return_dict = False)
+            epoch_loss = epoch_loss+batch_loss
+            if b %(batch_count/8) == 0:
+                print("...")
+        for v in range(validation_batch_count):
+            split_inp = []
+            inp = np.load(data_path+"Validation\\"+"input"+str(v)+".npy")
+            for i in range(inp.shape[3]):
+                split_inp.append(inp[:,:,:,i])
+            o = np.load(data_path+"Validation\\"+"output"+str(v)+".npy")
+            v_loss = model.test_on_batch(split_inp, o, return_dict = False)
+            validation_loss = validation_loss+v_loss
+            
+        epoch_loss = epoch_loss/batch_count
+        validation_loss = validation_loss/validation_batch_count
+        print("validation loss "+str(validation_loss))
+        print("loss "+str(epoch_loss))
+        
+    model.save('SequentialPreEncoded.model')
+
+#Ex 11) Load pre-encoded model and make prediction - Deprecated, data format changed
+def ex11(test_file_number):
+    import tensorflow.keras as keras
+
+    model = keras.models.load_model('SequentialAutoEncoder.model')
+    
+    data_path = "H:\\Datasets\\ChirpsDaily\\Sequential\\"
+    inputs = []
+    inp = np.load(data_path+"Validation\\"+"input"+str(test_file_number)+".npy")
+    for i in range(inp.shape[3]):
+        inputs.append(inp[:,:,:,i])
+    out = np.load(data_path+"Validation\\"+"output"+str(test_file_number)+".npy")
+    
+    pred = model.predict_on_batch(inputs)
+    f, im = plt.subplots(7)
+    for i in range(inp.shape[3]):
+        im[i].set_title("Input Data"+str(i))
+        im[i].imshow(inp[0,:,:,i])
+    im[5].set_title("Next Day Prediction")
+    im[5].imshow(pred[0,:,:,0])
+    im[6].set_title("Ground Truth")
+    im[6].imshow(out[0,:,:,0])
+    
+    plt.show()
+
+
+class CustomLoss(Loss):
+    @tf.function
+    def call(self, y_true, y_pred):
+        y_pred = tf.squeeze(y_pred)
+        diff = y_true-y_pred
+        #add on a multiple of absolute error
+        loss = keras.backend.abs(diff)+keras.backend.square(diff)
+        
+        # multiplying the values with weights along batch dimension
+        #loss = tf.math.multiply(loss, tf.reshape([1, 0.08, 0.06, 0.04, 0.02], (1,5,1,1)))       
+                    
+        # summing both loss values along batch dimension 
+        loss = keras.backend.sum(loss, axis=1)        
+        return loss
+
+#Ex 12) Load pre-encoded LSTM model and train the forecasting bottleneck
+def ex12(sequence_length):
+    loss = CustomLoss()
+    keras.losses.loss = loss
+    epochs = 1
+    sequence_length = 5
+    
+    
+    data_path = "H:\\Datasets\\ChirpsDaily\\Sequential\\"
+    batch_count = int(len(os.listdir(data_path+"Train\\"))/2)
+    validation_batch_count = int(len(os.listdir(data_path+"Validation\\"))/2)
+    print(batch_count)
+    
+    
+    
+    model =  Model.getModel((sequence_length, 360, 360, 1))
+    
+    # l = keras.models.load_model('MultiEncoder.model', compile = False)
+    # model.set_weights(l.get_weights())
+    # l = None
+
+    model.compile(optimizer='adam', loss=loss)
+    print(model.summary())
+    
+    for e in range(epochs):
+        print("Epoch "+str(e))
+        epoch_loss = 0;
+        validation_loss = 0;
+        train_date = date(1981,1,1)
+        validation_date = date(2021,1,1)
+        for b in range(batch_count):
+            inp = np.load(data_path+"Train\\"+"input"+str(b)+".npy")
+            out = np.load(data_path+"Train\\"+"output"+str(b)+".npy")
+            date_vec = getDateVector(train_date, inp.shape[0], sequence_length)
+            train_date = date_vec[1]
+            
+            batch_loss = model.train_on_batch([inp,np.zeros((inp.shape[0],1)), date_vec[0]], out[:,0,:,:], return_dict = False)
+            epoch_loss = epoch_loss+batch_loss
+            if b %(batch_count/8) == 0:
+                print("..." )#+ str(batch_loss))
+        for v in range(validation_batch_count):
+            inp = np.load(data_path+"Validation\\"+"input"+str(v)+".npy")
+            o = np.load(data_path+"Validation\\"+"output"+str(v)+".npy")
+            date_vec = getDateVector(validation_date, inp.shape[0], sequence_length)
+            validation_date = date_vec[1]
+            
+            v_loss = model.test_on_batch([inp,np.zeros((inp.shape[0],1)), date_vec[0]], o[:,0,:,:], return_dict = False)
+            validation_loss = validation_loss+v_loss
+            
+        epoch_loss = epoch_loss/batch_count
+        validation_loss = validation_loss/validation_batch_count
+        print("validation loss "+str(validation_loss))
+        print("loss "+str(epoch_loss))
+        
+    model.save('MultiEncoder.model')
+    
+#Ex 13) Load pre-encoded GRU model and make prediction
+def ex13(test_file_number):
+    validation_date = date(2021,1,1)+timedelta(days=test_file_number)
+    loss = CustomLoss()
+    keras.losses.loss = loss
+    model = keras.models.load_model('MultiEncoder.model', custom_objects={ 'CustomLoss': loss })
+    
+    data_path = "H:\\Datasets\\ChirpsDaily\\Sequential\\"
+    inp = np.load(data_path+"Validation\\"+"input"+str(test_file_number)+".npy")
+    out = np.load(data_path+"Validation\\"+"output"+str(test_file_number)+".npy")
+    
+    date_vec = getDateVector(validation_date, inp.shape[0], 5)
+    
+    pred = model.predict_on_batch([inp,np.zeros((inp.shape[0],1)), date_vec[0]])
+    f, im = plt.subplots(1,inp.shape[1]+2)
+    
+    
+    for i in range(inp.shape[1]):
+        im[i].set_title("Input Data"+str(i))
+        im[i].imshow(inp[0,i,:,:])
+    im[inp.shape[1]].set_title("Prediction")
+    im[inp.shape[1]].imshow(pred[0,0,:,:])
+    im[inp.shape[1]+1].set_title("Ground Truth")
+    im[inp.shape[1]+1].imshow(out[0,0,:,:])
+    plt.show()
 
 def main():
-    norm = batch_numpy_file_to_folder("ChirpsDaily.npy", "\\Sequential\\Train", 32, 5)
-    batch_numpy_file_to_folder_with_normalizer("ChirpsDaily2021.npy", "\\Sequential\\Validation", 1, 5, norm)
-    ex6(5)
-    ex7(1)
-    ex7(2)
-    ex7(3)
+    #norm = batch_numpy_file_to_folder("ChirpsDaily.npy", "\\Sequential\\Train", 32, 5)
+    #batch_numpy_file_to_folder_with_normalizer("ChirpsDaily2021.npy", "\\Sequential\\Validation", 1, 5, norm)
+    #generate_empty_data(64)
+    
+    #Train autoencoder
+    #ex8()
+    #ex9(0)
+    #ex9(15)
+    
+    #Train Latent Predictor
+    ex12(5)
+    ex13(0)
+    ex13(10)
+    ex13(20)
+    ex13(30)
+    
+    #Calculate and visualize dataset analysis
+    # stats = get_data_stats()
+    # splits = get_split_averages_on_data(40)
+    # visualize_splits_vs_avg(splits, stats[0],10,4)
+    # visualize_splits_vs_prev(splits, 10,4)
+    # visualize_splits_vs_first(splits, 10,4)
+    # splits = get_split_stdevs_on_data(40)
+    # visualize_splits_vs_avg(splits, stats[1],10,4)
     
 
     
--- Model.py
+++ Model.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten, concatenate, Reshape, Dropout
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, AveragePooling2D, UpSampling2D, Flatten, concatenate, Reshape, Dropout, LSTM, TimeDistributed, Concatenate, BatchNormalization
 from tensorflow.keras.models import Model
 
 def getAutoEncoder(input_shape):
@@ -109,6 +109,164 @@ def getSequentialAutoEncoder(input_shape):
     # Deconv3
     x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
     x = UpSampling2D((2, 2))(x)
-    decoded = Conv2D(1, (3, 3), activation='relu', padding='same')(x)
+    decoded = Conv2D(1, (3, 3), activation='linear', padding='same')(x)
     
     return Model(inputs, decoded)
+    
+def getSameDayAutoEncoder(input_shape):
+
+    input_img = Input(shape=input_shape)
+
+    # Conv1 #
+    x = Conv2D(filters = 16, kernel_size = (3, 3), activation='relu', padding='same')(input_img)
+    x = AveragePooling2D(pool_size = (2, 2), padding='same')(x)
+    # Conv2 #
+    x = Conv2D(filters = 8, kernel_size = (3, 3), activation='relu', padding='same')(x)
+    x = AveragePooling2D(pool_size = (2, 2), padding='same')(x)
+    # Conv 3 #
+    x = Conv2D(filters = 4, kernel_size = (3, 3), activation='relu', padding='same')(x)
+    encoded = AveragePooling2D(pool_size = (2, 2), padding='same')(x)
+    
+    encoder = Model(input_img, encoded)
+    
+    # DeConv1
+    decoder_input = Input(shape = (45,45,4))
+    x = Conv2D(4, (3, 3), activation='relu', padding='same')(decoder_input)
+    x = UpSampling2D((2, 2))(x)
+    # DeConv2
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    # Deconv3
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    decoded = Conv2D(1, (3, 3), activation='linear', padding='same')(x)
+    
+    decoder = Model(decoder_input, decoded)
+    
+    
+    autoencoder_input = Input(shape = input_shape)
+    e = encoder(autoencoder_input)
+    d = decoder(e)
+    autoencoder = Model(autoencoder_input, d)
+    
+    
+    return (autoencoder, encoder, decoder)
+    
+def getSequentialPreEncoded(input_shape):
+    import tensorflow.keras as keras
+
+    inputs = []
+    encoded = []
+    encoder = keras.models.load_model('encoder.model')
+    for layer in encoder.layers:
+        layer.trainable = False
+    decoder = keras.models.load_model('decoder.model')
+    for layer in decoder.layers:
+        layer.trainable = False
+    
+    #encoders for each input
+    for i in range(input_shape[2]):
+        inputs.append(Input(shape=(input_shape[0], input_shape[1], 1)))
+        encoded.append(encoder(inputs[i]))
+
+    # c = []
+    # for i in range(input_shape[2]):
+        # c.append(Flatten()(encoded[i]))
+    # x = concatenate([c[0], c[1], c[2], c[3], c[4]])
+    
+    #Forecast
+    x = Flatten()(encoded[0])
+    x=Dense(units = 500, activation = 'sigmoid')(x)
+    x=Dropout(rate = .5)(x)
+    for i in range(input_shape[2]-1):
+        flt = Flatten()(encoded[i+1])
+        combined = concatenate([x, flt])
+        x=Dense(units = 500+400*(i+1), activation = 'sigmoid')(combined)
+        x=Dropout(rate = .5)(x)
+    x = Dense(units = 2025, activation = 'sigmoid')(x)
+    x = Reshape((45,45, 1))(x)
+    forecast = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    
+    decoded = decoder(forecast)
+    
+    return Model(inputs, decoded)
+    
+def getModel(input_shape):
+    import tensorflow.keras as keras
+    import numpy as np
+
+    encoder = keras.models.load_model('encoder.model')
+    for layer in encoder.layers:
+        layer.trainable = False
+    decoder = keras.models.load_model('decoder.model')
+    for layer in decoder.layers:
+        layer.trainable = False
+    
+    #encoders for each input
+    inp = Input(shape=input_shape)
+    x = TimeDistributed(encoder)(inp)
+    x = Reshape((5,8100))(x)
+    
+    date_encoding = Input(shape = (5,13))
+    x = Concatenate(axis=2)([x,date_encoding])
+    
+    
+    
+    
+    output_len = 1
+    regulariser = None
+    hidden_shape = [10,10,10,10,10]
+    ts_decoder_inputs = Input(shape=(output_len, 1))
+
+    ts_encoder_inputs = keras.layers.Input(shape=(5,8113))
+    # Create a list of RNN Cells, these are then concatenated into a single layer
+    # with the RNN layer.
+    ts_encoder_cells = []
+    for hidden_layers in hidden_shape:
+        ts_encoder_cells.append(keras.layers.GRUCell(hidden_layers,
+                                                  kernel_regularizer=regulariser,
+                                                  recurrent_regularizer=regulariser,
+                                                  bias_regularizer=regulariser))
+                                                  
+    ts_encoder = keras.layers.RNN(ts_encoder_cells, return_state=True)
+    
+    ts_encoder_outputs_and_states = ts_encoder(ts_encoder_inputs)
+    ts_encoder_states = ts_encoder_outputs_and_states[1:]
+    
+    
+    
+    
+
+    ts_decoder_cells = []
+    for hidden_neurons in hidden_shape:
+        ts_decoder_cells.append(keras.layers.GRUCell(hidden_neurons,
+                                                  kernel_regularizer=regulariser,
+                                                  recurrent_regularizer=regulariser,
+                                                  bias_regularizer=regulariser))
+    ts_decoder = keras.layers.RNN(ts_decoder_cells, return_sequences=True, return_state=True)
+    
+    ts_decoder_outputs_and_states = ts_decoder(ts_decoder_inputs, initial_state=ts_encoder_states)
+    ts_decoder_outputs = ts_decoder_outputs_and_states[0]
+    ts_decoder_outputs = BatchNormalization()(ts_decoder_outputs)
+    ts_decoder_dense = keras.layers.Dense(8100,
+                                       activation='linear',
+                                       kernel_regularizer=regulariser,
+                                       bias_regularizer=regulariser)
+    ts_decoder_outputs = ts_decoder_dense(ts_decoder_outputs)
+    tsed = keras.models.Model(inputs=[ts_encoder_inputs, ts_decoder_inputs], outputs=ts_decoder_outputs)
+    print(tsed.output_shape)
+    print(tsed.summary())     
+    x = tsed([x,ts_decoder_inputs])     
+         
+         
+    print(x.shape)     
+         
+         
+         
+    #x = Dense(units = 8100, activation = 'linear')(x)
+    x = Reshape((output_len, 45,45, 4))(x)
+    
+    decoded = TimeDistributed(decoder)(x)
+    
+    return Model([inp, ts_decoder_inputs, date_encoding], decoded)
+    
